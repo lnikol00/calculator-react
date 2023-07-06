89d93298784f8a1b0abe68e767d98de8920dc1e4
Koce1 <122328343+lnikol00@users.noreply.github.com>
2023-07-06 14:18:16 +0200

    final scss update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="app-container">
-      <h1>Kalkulator</h1>
       <Home />
     </div>
   );
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,20 +1,33 @@
 import "./home.scss"
-import { Buttons } from "./ButtonData"
 
 function Home() {
     return (
         <div className="main-container">
             <div className="calculator-container">
                 <div className="screen-container">
-                    <input />
+                    <span>0</span>
                 </div>
 
                 <div className="button-container">
-                    {Buttons.map((data) => {
-                        return (
-                            <button key={data.id}>{data.button}</button>
-                        )
-                    })}
+                    <button>C</button>
+                    <button>+/-</button>
+                    <button>%</button>
+                    <button>/</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>x</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>-</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>+</button>
+                    <button>0</button>
+                    <button>,</button>
+                    <button>=</button>
                 </div>
             </div>
         </div>
